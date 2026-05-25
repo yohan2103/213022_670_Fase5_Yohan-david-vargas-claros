c2ecf7d679b9e07b42f29a3d2429c840e1063447
Yohan David Vargas Claros <ydvargascl@unadvirtual.edu.co>
2026-05-25 14:59:19 -0500

    corrección de estructura del inventario

--- Fase_5_problema_#_3.py
+++ Fase_5_problema_#_3.py
@@ -1,27 +1,28 @@
 
 # Función que calcula cuántas unidades se deben pedir
 def calcular_pedido(actual, minimo):
-    if actual < minimo:
+    if actual < minimo: 
         return minimo - actual
     return 0
 
 
 # Matriz de inventario
 inventario = [
-    ["001, Arroz", 10, 20],
-    ["002, Azúcar", 25, 20],
-    ["003, Aceite", 5, 15],
-    ["004, Sal", 30, 25],
-    ["005, Harina", 8, 12]
+    ["001", "Arroz", 10, 20],
+    ["002", "Azúcar", 25, 20],
+    ["003", "Aceite", 5, 15],
+    ["004", "Sal", 30, 25],
+    ["005", "Harina", 8, 12]
 ]
 
 print("LISTA DE PEDIDOS")
 
-for articulo in inventario:
-    nombre = articulo[0]
-    stock_actual = articulo[1]
-    stock_minimo = articulo[2]
+for articulo in inventario: 
+    codigo = articulo[0]
+    nombre = articulo[1]
+    stock_actual = articulo[2]
+    stock_minimo = articulo[3]
 
     pedido = calcular_pedido(stock_actual, stock_minimo)
 
-    print(nombre, "-> Cantidad a pedir:", pedido)
+    print( codigo, nombre, "-> Cantidad a pedir:", pedido)
